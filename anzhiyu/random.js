afc5dec5f767225a2cd92153da294ec92c03d7ad
mnmmw <mengnanmmw@gmail.com>
2025-12-18 13:05:35 +0800

    Site updated: 2025-12-18 13:05:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["64080/","2725/"];function toRandomPost(){
+var posts=["2725/","64080/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
